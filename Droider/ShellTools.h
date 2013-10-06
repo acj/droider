a02d336b8c56287db89c8d47642724033a650b7a
Adam Jensen <acj@linuxguy.org>
2013-10-06 16:26:00 -0400

    Add skeleton ShellTools class

---
+++ Droider/ShellTools.h
@@ -0,0 +1,13 @@
+//
+//  ShellTools.h
+//  Droider
+//
+//  Created by Adam Jensen on 10/6/13.
+//  Copyright (c) 2013 Adam Jensen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShellTools : NSObject
+
+@end
---
+++ Droider/ShellTools.m
@@ -0,0 +1,13 @@
+//
+//  ShellTools.m
+//  Droider
+//
+//  Created by Adam Jensen on 10/6/13.
+//  Copyright (c) 2013 Adam Jensen. All rights reserved.
+//
+
+#import "ShellTools.h"
+
+@implementation ShellTools
+
+@end
