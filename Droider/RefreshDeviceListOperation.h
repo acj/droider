a010761212ddb40abea230ccf39286db3c1fbbb3
Adam Jensen <acj@linuxguy.org>
2013-12-08 18:27:29 -0500

    Add RefreshDeviceListOperation class skeleton

---
+++ Droider/RefreshDeviceListOperation.h
@@ -0,0 +1,13 @@
+//
+//  RefeshDeviceListOperation.h
+//  Droider
+//
+//  Created by Adam Jensen on 12/8/13.
+//  Copyright (c) 2013 Adam Jensen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RefeshDeviceListOperation : NSObject
+
+@end
---
+++ Droider/RefreshDeviceListOperation.m
@@ -0,0 +1,13 @@
+//
+//  RefeshDeviceListOperation.m
+//  Droider
+//
+//  Created by Adam Jensen on 12/8/13.
+//  Copyright (c) 2013 Adam Jensen. All rights reserved.
+//
+
+#import "RefeshDeviceListOperation.h"
+
+@implementation RefeshDeviceListOperation
+
+@end
